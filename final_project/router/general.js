e7b52050c44e7d385e49bc84ed71e885230c9a19
Phani170 <71382398+Phani170@users.noreply.github.com>
2024-07-21 10:55:38 -0400

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,39 +5,96 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+public_users.post("/register", function(req,res) {
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+
+  if (username && password) {
+      
+      if (!isValid(username)) {
+          
+          users.push({"username": username, "password": password});
+          return res.status(200).json({message: "User successfully registered. Now you can login"});
+      } else {
+          return res.status(404).json({message: "User already exists!"});
+      }
+  }
+  
+  return res.status(404).json({message: "Unable to register user."});
+  
 });
 
-// Get the book list available in the shop
+
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
+  let myPromise = new Promise((resolve,reject) => {
+    setTimeout(() => {
+      resolve(res.send(JSON.stringify(books,null,11)));
+    },6000)})
+
+    console.log("Before calling promise");
+
+    myPromise.then((successMessage) => {
+      console.log("From Callback " + successMessage)
+    })
+    console.log("After calling promise");
 });
 
-// Get book details based on ISBN
+
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  const isbn = req.params.isbn;
+  let myPromise = new Promise((resolve,reject) => {
+    setTimeout(() => {
+      resolve(res.send(books[isbn]));
+    },6000)})
+
+    console.log("Before calling promise");
+
+    myPromise.then((successMessage) => {
+      console.log("From Callback " + successMessage)
+    })
+    console.log("After calling promise");
+
  });
   
-// Get book details based on author
+
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let ele=1;
+  Object.entries(books).forEach(([key, book]) => {
+    if (book.author === author) {
+      res.send(books[ele])
+    }
+    ele+=1;
+  });
+
+  res.status(300).json({message: "Author not found"});
 });
 
-// Get all books based on title
+
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+ 
+  const title = req.params.title;
+  let ele=1;
+  Object.entries(books).forEach(([key, book]) => {
+    if (book.title === title) {
+      res.send(books[ele]);
+    }
+    ele+=1;
+  });
+  return res.status(300).json({message: "Book not found"});
 });
 
-//  Get book review
+
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  const isbn = req.params.isbn;
+  const reviews = books[isbn].reviews;
+  res.send(reviews);
 });
 
 module.exports.general = public_users;
